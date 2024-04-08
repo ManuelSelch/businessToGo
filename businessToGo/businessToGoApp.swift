8dc8458789588bf863c75112473e345db2a7ec85
Manuel Selch <manuel@selch.de>
2024-04-08 13:39:32 +0200

    Initial Commit

---
+++ businessToGo/businessToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessToGoApp.swift
+//  businessToGo
+//
+//  Created by Admin  on 08.04.24.
+//
+
+import SwiftUI
+
+@main
+struct businessToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
